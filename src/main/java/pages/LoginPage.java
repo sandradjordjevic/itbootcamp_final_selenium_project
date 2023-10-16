45d4071b89e7a0ef51b0c99dd6892fe7b00f0892
Sandra Djordjevic <sandradj2331@gmail.com>
2023-10-16 21:03:12 +0200

    Add Login Page

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage extends BasicPage{
+    public LoginPage(WebDriver driver, WebDriverWait wait) {
+        super(driver, wait);
+    }
+    public WebElement getEmailInput () {
+        return driver.findElement(By.id("email"));
+    }
+    public void clearAndTypeEmail (String email) {
+        getEmailInput().clear();
+        getEmailInput().sendKeys(email);
+    }
+    public WebElement getPasswordInput () {
+        return driver.findElement(By.id("password"));
+    }
+    public void clearAndTypePassword (String password) {
+        getPasswordInput().clear();
+        getPasswordInput().sendKeys(password);
+    }
+    public WebElement getLoginButton () {
+        return driver.findElement(By.cssSelector("button[fdprocessedid='5l15pd']"));
+    }
+    public void clickOnLoginButton () {
+        getLoginButton().click();
+    }
+}
