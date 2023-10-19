f9f94af1ecc1ef5a75e5f23ab44ab063f5e7a19b
Sandra Djordjevic <sandradj2331@gmail.com>
2023-10-19 21:29:09 +0200

    Check input types

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -21,4 +22,48 @@ public class ProfilePage extends BasicPage{
         wait    .withMessage("Load dialog should be invisible after a few seconds")
                 .until(ExpectedConditions.invisibilityOf(driver.findElement(By.cssSelector("div.v-dialog.v-dialog--persistent"))));
     }
+    public String getAttributeTypeFromInputEmail() {
+        return getInputEmail().getAttribute("type");
+    }
+    public String getAttributeDisabledFromInputEmail () {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return js.executeScript("return arguments[0].getAttribute('disabled');", getInputEmail()).toString();
+    }
+    public WebElement getInputName () {
+        return driver.findElement(By.id("name"));
+    }
+    public String getAttributeTypeFromInputName () {
+        return getInputName().getAttribute("type");
+    }
+    public WebElement getInputCity () {
+        return driver.findElement(By.id("city"));
+    }
+    public String getAttributeTypeFromInputCity () {
+        return getInputCity().getAttribute("type");
+    }
+    public WebElement getInputCountry () {
+        return driver.findElement(By.id("country"));
+    }
+    public String getAttributeTypeFromInputCountry () {
+        return getInputCountry().getAttribute("type");
+    }
+    public WebElement getInputTwitter () {
+        return driver.findElement(By.id("urlTwitter"));
+    }
+    public String getAttributeTypeFromInputTwitter () {
+        return getInputTwitter().getAttribute("type");
+    }
+    public WebElement getInputGitHub () {
+        return driver.findElement(By.id("urlGitHub"));
+    }
+    public String getAttributeTypeFromInputGitHub () {
+        return getInputGitHub().getAttribute("type");
+    }
+    public WebElement getInputPhone () {
+        return driver.findElement(By.id("phone"));
+    }
+    public String getAttributeTypeFromInputPhone () {
+        return getInputPhone().getAttribute("type");
+    }
+
 }
--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -20,4 +20,31 @@ public class ProfileTests extends BasicTest{
         Assert.assertEquals(email,
                 profilePage.getAttributeValueFromInputEmail());
     }
+    @Test (priority = 2, retryAnalyzer = RetryAnalyzer.class)
+    public void checksInputTypes () throws InterruptedException {
+        String email = "admin@admin.com";
+        String password = "12345";
+
+        navPage.clickOnTheLoginButton();
+        loginPage.login(email, password);
+        navPage.clickOnTheMyProfileButton();
+        profilePage.waitForTheDialogToBeInvisible();
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputEmail().equals("email"),
+                "Input email should have email type");
+        Assert.assertTrue(profilePage.getAttributeDisabledFromInputEmail().equals("disabled"),
+                "Input email should be disabled");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputName().equals("text"),
+                "Input name should have text type");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputCity().contains("text"),
+                "Input city should have text type");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputCountry().contains("text"),
+                "Input country should have text type");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputTwitter().contains("url"),
+                "Input twitter should have url type");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputGitHub().contains("url"),
+                "Input github should have url type");
+        Assert.assertTrue(profilePage.getAttributeTypeFromInputPhone().contains("tel"),
+                "Input phone should have tel type");
+        navPage.clickOnTheLogOutButton();
+    }
 }
